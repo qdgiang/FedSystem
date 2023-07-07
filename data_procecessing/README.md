c669bc67e9b5de6b2d1dbc0957c06389c33b6542
Giang <55189671+qdgiang@users.noreply.github.com>
2023-07-07 16:20:52 +0700

    Initial commit

---
+++ data_procecessing/README.md
@@ -0,0 +1,3 @@
+The data in this folder is processed independently.
+After processing, it should be moved to the data folder of the client. Server should not have access to any data.
+In real life, this should already be done.
---
+++ server/server.py
@@ -0,0 +1,20 @@
+import flwr as fl
+from strategy import base_strategy
+import yaml
+
+def main():
+
+    with open("server.yaml", "r") as f:
+        server_config = yaml.safe_load(f)
+
+
+    # Start Flower server for four rounds of federated learning
+    fl.server.start_server(
+        server_address="0.0.0.0:8080",
+        config=fl.server.ServerConfig(num_rounds=10),
+        strategy=base_strategy,
+    )
+
+
+if __name__ == "__main__":
+    main()
